80ff2cd18df2c2e8e83686bf144795ae73097b57
shreyansh-singh74 <1032230662@tcetmumbai.in>
2025-04-29 21:24:29 +0530

    Implement brand logos display and enhance WhatGPT3 section layout

--- my-app/src/containers/whatGPT3/WhatGPT3.jsx
+++ my-app/src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import './whatGPT3.css'
+import { Feature } from '../../components'
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
+    <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+      <div className='gpt3__whatgpt3-feature'>
+        <Feature />
+      </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient-text'>The possibilities beyond your imagination</h1>
+        <p>Explore the Library</p>
+      </div>
+      <div className='gopt3__whatgpt3-container'>
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    </div>
   )
 }
 
