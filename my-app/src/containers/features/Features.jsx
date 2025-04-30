0caa16541af7d92ed4bb71c8a2b25bf3acb2e00c
shreyansh-singh74 <1032230662@tcetmumbai.in>
2025-04-30 23:06:45 +0530

    Enhance Features and WhatGPT3 components with dynamic data and improved styling - (Added new featues )

--- my-app/src/containers/features/Features.jsx
+++ my-app/src/containers/features/Features.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
 import './features.css'
+import {Feature} from '../../components'
+
+
 const Features = () => {
+
+  const featuresData = [
+    {
+      title: 'Improving end distrust instantly',
+      text: 'From they fine john he give of rich he. They age around much. They have you the most beautiful in the world.',
+    },
+    {
+      title: 'Empowering the Future',
+      text: 'The future belongs to those who believe in the beauty of their dreams.',
+    },
+    {
+      title: 'Innovating for Tomorrow',
+      text: 'Innovation distinguishes between a leader and a follower.',
+    },
+    {
+      title: 'Revolutionizing the Industry',
+      text: 'The only way to do great work is to love what you do.',
+    },
+  ]
+
   return (
-    <div>Features</div>
+    <div className='gpt3__features section__padding' id='features'>
+      <div className='gpt3__features-heading'>
+        <h1 className='gradient_text'>The Future is Now and You Just Need to Realize it. Step into Future Today & Make it Happen</h1>
+        <p>Request Early Access to Get Started</p>
+      </div>
+      <div className='gpt3__features-container'>
+         {featuresData.map((item,index) => (
+          <Feature key={index} title={item.title} text={item.text} />
+         ))}
+      </div>
+    </div>
   )
 }
 
--- my-app/src/containers/whatGPT3/WhatGPT3.jsx
+++ my-app/src/containers/whatGPT3/WhatGPT3.jsx
@@ -5,16 +5,16 @@ const WhatGPT3 = () => {
   return (
     <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
       <div className='gpt3__whatgpt3-feature'>
-        <Feature />
+        <Feature title={"What is GPT-3"} text={"Advanced AI language model that understands and generates human-like text. Advanced AI language model that understands and generates human-like text"}/>
       </div>
       <div className='gpt3__whatgpt3-heading'>
-        <h1 className='gradient-text'>The possibilities beyond your imagination</h1>
+        <h1 className='gradient_text'>The possibilities beyond your imagination</h1>
         <p>Explore the Library</p>
       </div>
-      <div className='gopt3__whatgpt3-container'>
-        <Feature />
-        <Feature />
-        <Feature />
+      <div className='gpt3__whatgpt3-container'>
+        <Feature title={"Chatbots"} text={"Create intelligent conversational agents that respond naturally to inquiries."}/>
+        <Feature title={"Knowledge Base"} text={"Access vast information networks to solve complex problems efficiently."}/>
+        <Feature title={"Education"} text={"Transform learning experiences with personalized AI-powered educational content."}/>
       </div>
     </div>
   )
